37be74c627a139f7c97808c072c8b443b7ae506c
howpwn <finn79426@gmail.com>
2018-07-13 22:57:36 +0800

    新增備份 Sncript

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+cp ~/.vimrc $(dirname "$0")/vimrc
+echo 'vimrc Done!\n'
+
+cp ~/.zshrc $(dirname "$0")/zshrc
+echo 'zshrc Done!\n'
+
+cp ~/.gdbinit $(dirname "$0")/gdbinit
+echo 'gdbinit Done!\n'
+
+echo 'ALL Done!\nSave to :' $(dirname "$0")
